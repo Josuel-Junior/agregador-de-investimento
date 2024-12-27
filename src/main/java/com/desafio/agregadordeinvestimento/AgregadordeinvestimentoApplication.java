dc25fa74270e9ba00271d6a66c6288b23683f9ef
Josuel Junior <108230374+Josuel-Junior@users.noreply.github.com>
2024-12-27 17:03:39 -0300

    commit initial

---
+++ src/main/java/com/desafio/agregadordeinvestimento/AgregadordeinvestimentoApplication.java
@@ -0,0 +1,13 @@
+package com.desafio.agregadordeinvestimento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgregadordeinvestimentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgregadordeinvestimentoApplication.class, args);
+	}
+
+}
